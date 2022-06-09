287866eec6e2bfcea16282c272e2b39ddb43cf03
DESKTOP-INMFNVM\gaura <gaurangbhatt19@gmail.com>
2022-06-09 17:47:12 +0530

    Registration Login Premium Library grid password view navbar isuue

--- views/navbar/navbar.js
+++ views/navbar/navbar.js
@@ -1,20 +1,10 @@
-var navbar=angular.module('app.navbar',['ngMaterial', 'ngMessages'])
+var navbar=angular.module('app.navbar',['ngMaterial','ngMessages'])
 
-navbar.factory('nav_value',['$location',function($location) {
- var currentPath=""
- console.log(currentPath);
- var navbar={};
- navbar.getPath=function(){
-     return currentPath
- }
+navbar.controller('AppCtrl',["$scope","$location",'$rootScope',function($scope,$location,$rootScope){
+     
 
- navbar.setPath=function(path){
-     currentPath=path
- }
-
- return navbar
-}])
-
-navbar.controller('AppCtrl',['nav_value',"$scope","$location",function(nav_value,$scope,$location){
-     $scope.currentNavItem=nav_value.getPath().substring(1,)
+     $rootScope.$on('$routeChangeSuccess', function () {
+         let value=$location.path().substring(1,)
+         $scope.currentNavItem=value
+    });
 }])
